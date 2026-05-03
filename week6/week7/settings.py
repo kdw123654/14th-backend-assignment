d7b86c4f3b1c6c38199189e44807ca76a8835a07
infiniteDipia <kimk39257@gmail.com>
2026-05-03 14:33:36 +0900

    week9/kimdongwook

--- week6/week7/settings.py
+++ week6/week7/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-        'USER': 'root',                      #
+        'USER': 'kimk1',                      #
         'PASSWORD': 'your_password',         
         'HOST': '127.0.0.1',             
 }
